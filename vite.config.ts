c49d13f9e34e6b70afb022aad79ed5be78fca76f
Max Parker <maxim.j.parker@gmail.com>
2024-10-20 23:03:54 +0000

    build: fix build script

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,17 @@
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig(() => ({
-  plugins: [vue()],
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      formats: ['es'],
+      entry: fileURLToPath(new URL('./src/index.ts', import.meta.url))
+    }
+  },
   test: {
     environment: 'happy-dom',
     browser: {
